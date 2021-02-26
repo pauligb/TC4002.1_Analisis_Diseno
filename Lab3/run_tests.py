684cb1dbbc2871e0baf8855e064e980edbee7554
Paul Gallegos <paul.igb@gmail.com>
2021-02-25 23:00:37 -0800

    Adding placeholders for Lab3

---
+++ Lab3/run_tests.py
@@ -0,0 +1,13 @@
+import sys
+import os
+import unittest
+
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '../src')))
+
+loader = unittest.TestLoader()
+start_dir = 'test/'
+suite = loader.discover(start_dir)
+
+runner = unittest.TextTestRunner()
+runner.run(suite)
---
+++ Lab3/src/records_handler.py
@@ -0,0 +1,6 @@
+class RecordsHandler:
+  def __init__(self, name = '', email = '', age = '', origin = ''):
+    self.name = name
+    self.email = email
+    self.age = age
+    self.origin = origin

8a81a0faab1b77f5e55e951a959ee9c3c08b99ed
Paul Gallegos <paul.igb@gmail.com>
2021-03-27 23:39:03 -0700

    Adding coverage and styling
    Removing unused code from errors, need functions to throw errors.
    
    Adding only one case when an error is thrown and how to catch it,
    not added the others because this was only used for purposed of
    learning.
    
    Adding coverage reporting.
    
    Adding example in how to mock in python.

--- Lab3/src/records_handler.py
+++ Lab3/src/records_handler.py
@@ -1,5 +1,4 @@
 import sqlite3
-from sqlite3 import Error
 
 
 class RecordsHandler:
@@ -17,10 +16,8 @@ class RecordsHandler:
     def create_connection(self, db_file):
         """ create a database connection to a SQLite database """
         self.conn = None
-        try:
-            self.conn = sqlite3.connect(db_file)
-        except Error as e:
-            print(e)
+
+        self.conn = sqlite3.connect(db_file)
 
         self.create_table(self.sql_create_projects_table)
 
@@ -34,34 +31,27 @@ class RecordsHandler:
         :param create_table_sql: a CREATE TABLE statement
         :return:
         """
-        try:
-            c = self.conn.cursor()
-            c.execute(create_table_sql)
-            self.conn.commit()
-        except Error as e:
-            print(e)
+        c = self.conn.cursor()
+        c.execute(create_table_sql)
+        self.conn.commit()
 
     def add_record(self, name="", email="", age="", origin=""):
         sqlquery = "INSERT INTO Records (name, email, age, origin) VALUES ('%s', '%s', '%s', '%s')"
         val = (name, email, age, origin)
         query = sqlquery % val
-        try:
-            c = self.conn.cursor()
-            c.execute(query)
-            self.conn.commit()
-        except Error as e:
-            print(e)
+
+        c = self.conn.cursor()
+        c.execute(query)
+        self.conn.commit()
 
     def delete_record(self, record_id=-1):
         sqlquery = "DELETE FROM Records WHERE id='%s'"
         val = record_id
         query = sqlquery % val
-        try:
-            c = self.conn.cursor()
-            c.execute(query)
-            self.conn.commit()
-        except Error as e:
-            print(e)
+
+        c = self.conn.cursor()
+        c.execute(query)
+        self.conn.commit()
 
     def look(self, email="", age=""):
         sqlquery = "SELECT * FROM Records WHERE email='%s' AND age='%s'"
@@ -69,34 +59,28 @@ class RecordsHandler:
         query = sqlquery % val
 
         results = []
-        try:
-            c = self.conn.cursor()
-            c.execute(query)
-            results.extend(c.fetchall())
-        except Error as e:
-            print(e)
+
+        c = self.conn.cursor()
+        c.execute(query)
+        results.extend(c.fetchall())
 
         return results
 
     def list_all(self):
         query = "SELECT * FROM Records"
         results = []
-        try:
-            c = self.conn.cursor()
-            c.execute(query)
-            results.extend(c.fetchall())
-        except Error as e:
-            print(e)
+
+        c = self.conn.cursor()
+        c.execute(query)
+        results.extend(c.fetchall())
 
         # loop through the rows
         for row in results:
             print(row)
-    
+
     def delete_all(self):
         sqlquery = "DELETE FROM Records"
-        try:
-            c = self.conn.cursor()
-            c.execute(sqlquery)
-            self.conn.commit()
-        except Error as e:
-            print(e)
+
+        c = self.conn.cursor()
+        c.execute(sqlquery)
+        self.conn.commit()
